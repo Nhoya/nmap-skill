401440a5c76f7557b0576430945c24609ddc892f
Nhoya <nhoyadx@gmail.com>
2018-03-22 10:50:47 +0100

    Added nmap to dependencies

---
+++ requirements.sh
@@ -0,0 +1,23 @@
+# The requirements.sh is an advanced mechanism an should rarely be needed.
+# Be aware that it won't run with root permissions and 'sudo' won't work
+# in most cases.
+
+#detect distribution using lsb_release (may be replaced parsing /etc/*release)
+dist=$(lsb_release -ds)
+
+#setting dependencies and package manager in relation to the distribution
+if [ "$dist"  == "\"Arch Linux\""  ]; then
+    pm="pacman -S"
+elif [[ "$dist" =~  "Ubuntu" ]] || [[ "$dist" =~ "Debian" ]]; then
+    pm="apt install"
+elif [[ "$dist" =~ "SUSE" ]]; then 
+    pm="zypper install"
+fi
+
+dependencies=( nmap )
+
+# installing dependencies
+for dep in "${dependencies[@]}"
+do
+    sudo $pm $dep
+done
