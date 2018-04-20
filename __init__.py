7f9fac80c8135cfceaadb0703fb565efe9208349
Nhoya <nhoyadx@gmail.com>
2018-04-20 19:08:14 +0200

    Testing local scan

--- __init__.py
+++ __init__.py
@@ -11,18 +11,27 @@ class NmapSkill(MycroftSkill):
         scan_intent = IntentBuilder("ScanIntent"). \
             require("ScanKeyword").require("Host").build()
         self.register_intent(scan_intent, self.handle_scan_intent)
+        
+        local_scan = IntentBuilder("LocalScan"). \
+            require("LocalScanKeyword")
+        self.register_intent(local_scan, self.handle_local_scan)
 
-
-    def handle_scan_intent(self, message):
+    def handle_scan_intent(self , message):
         h = message.data.get('utterance').replace('scan ','').replace(' ', '')
-        self.speak('Port scanner started on '+h)
+        self.nmap_scan(h)
+
+    def handle_local_scan(self, message):
+        self.nmap_scan("192.168.1.*")
+
+    def nmap_scan(self,h):
+        self.speak("Scan started, this may take a while")
         nm=nmap.PortScanner()
         nm.scan(h, arguments='-sT')
         if not nm.all_hosts():
             self.speak("Unable to retrieve info on target")
             return
         for host in nm.all_hosts():
-            # print(k)
+            self.speak(host+" found")
             for proto in nm[host].all_protocols():
                 ports = sorted(nm[host][proto].keys())
                 for port in ports:
